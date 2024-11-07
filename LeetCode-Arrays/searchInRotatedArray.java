d0bb5a5f277788b742b308666ac7b05ce34e1f08
Vishaljat5112 <153274029+Vishaljat5112@users.noreply.github.com>
2024-11-07 17:05:40 +0530

    Add files via upload

---
+++ LeetCode-Arrays/searchInRotatedArray.java
@@ -0,0 +1,37 @@
+public class searchInRotatedArray  {
+
+    public static int search(int[] nums, int target) {
+        // We are using binary search for this:
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[start] <= nums[mid]) {  // Check if left half is sorted
+                if (nums[start] <= target && target < nums[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {  // Right half is sorted
+                if (nums[mid] < target && target <= nums[end]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+        }
+        return -1;  // Target not found
+    }
+
+    public static void main(String[] args) {
+        int nums[] = {4, 5, 6, 7, 0, 1, 2};
+        int target = 0;
+        System.out.println(search(nums, target));  // Should output 4
+    }
+}
